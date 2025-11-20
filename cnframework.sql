b98c122c5b649abdab7b0ba0d2069880fd98ee51
ChernyyOrel <victor.buzenco@epitech.eu>
2025-11-20 21:59:42 +0100

    Update : default sql

--- cnframework.sql
+++ cnframework.sql
@@ -15,11 +15,11 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Listage de la structure de la base pour cleanfx
-CREATE DATABASE IF NOT EXISTS `cleanfx` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
-USE `cleanfx`;
+-- Listage de la structure de la base pour cnframework
+CREATE DATABASE IF NOT EXISTS `cnframework` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `cnframework`;
 
--- Listage de la structure de table cleanfx. ban
+-- Listage de la structure de table cnframework. ban
 CREATE TABLE IF NOT EXISTS `ban` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `player_id` int(11) NOT NULL,
@@ -34,18 +34,32 @@ CREATE TABLE IF NOT EXISTS `ban` (
 
 -- Les données exportées n'étaient pas sélectionnées.
 
--- Listage de la structure de table cleanfx. player
+-- Listage de la structure de table cnframework. character
+CREATE TABLE IF NOT EXISTS `character` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `player_id` int(11) NOT NULL,
+  `data` text NOT NULL DEFAULT '[]',
+  `skin` text NOT NULL DEFAULT '[]',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `FK_character_player` (`player_id`),
+  CONSTRAINT `FK_character_player` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table cnframework. player
 CREATE TABLE IF NOT EXISTS `player` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `discord_id` varchar(18) NOT NULL,
+  `discord_id` varchar(19) NOT NULL,
   `name` varchar(50) NOT NULL,
   `data` text NOT NULL DEFAULT '[]',
   `roles` text NOT NULL DEFAULT '["user"]',
-  `last_connection` int(11) NOT NULL,
+  `last_connection` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `discord_id` (`discord_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=70 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Les données exportées n'étaient pas sélectionnées.
 
