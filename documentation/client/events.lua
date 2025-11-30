d4c7619c03af0431ba7db08335cf34a0259b8032
ChernyyOrel <victor.buzenco@epitech.eu>
2025-11-30 17:58:57 +0100

    Documentation : updated

---
+++ documentation/client/events.lua
@@ -0,0 +1,6 @@
+-- CNFramework:client:updatePlayerData
+-- Description : Triggered when the playerData has been updated in framework's ClientCache.
+-- Source : server
+-- data : table
+RegisterNetEvent("CNFramework:client:updatePlayerData", function(data)
+end)
---
+++ documentation/server/events.lua
@@ -0,0 +1,5 @@
+-- CNFramework:server:playerClientHasLoaded
+-- Description : Triggered when the player first spawns on the server. (should not be used)
+-- source : client
+RegisterNetEvent("CNFramework:server:playerClientHasLoaded", function()
+end)
