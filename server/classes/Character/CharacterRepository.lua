bfdd4ef8e124ad021c0128c8248e124f6c308231
Victor <victor.buzenco@epitech.eu>
2025-12-09 13:32:15 +0100

    Chore : CreateCharacter method

--- server/classes/Character/CharacterRepository.lua
+++ server/classes/Character/CharacterRepository.lua
@@ -11,7 +11,52 @@ function Repository:getCharacters() -- table
     return self.private.characters
 end
 
-function Repository:createCharacter(playerId, data, metadata, skin) -- Character / false
+function Repository:createCharacter(playerId, firstName, lastName, gender, dob) -- Character / nil
+    -- playerId
+    if not CNF.methods.ValidateType(playerId, "number") or playerId < 1 then
+        error("CharacterRepository:createCharacter invalid playerId input.")
+    end
+
+    -- player with this id exists ?
+    local player = CNF.repositories["Player"]:getPlayerById(playerId)
+
+    if player == nil then
+        error("CharacterRepository:createCharacter player with this id doesn't exist.")
+    end
+
+    local characterData = {
+        data = {
+            firstName = firstName,
+            lastName = lastName,
+            gender = gender,
+            dob = dob,
+        },
+        metadata = {
+            registrationTime = os.time(),
+        },
+        skin = {},
+    }
+
+
+        -- New Player
+    local id = MySQL.insert.await("INSERT INTO `"..self.private.tableName.."` (player_id, data, metadata, skin) VALUES (@playerId, @data, @metadata, @skin)", {
+        ["playerId"] = playerId,
+        ["data"] = json.encode(characterData.data),
+        ["metadata"] = json.encode(characterData.metadata),
+        ["skin"] = json.encode(characterData.skin),
+    })
+
+    if id then
+        -- Init Character object
+        local character = CNF.classes["Character"]:new(id, playerId, characterData.data, characterData.metadata, characterData.skin)
+        
+        -- Adds the character to the repository
+        self:addCharacter(character)
+    
+        return character
+    else
+        return false
+    end
 end
 
 -- characterObject : Player
---
+++ server/commands/createcharacter.lua
@@ -0,0 +1,12 @@
+RegisterCommand("createcharacter", function(source, args, rawCommand)
+    local src = source
+    local firstName = args[1]
+    local lastName = args[2]
+
+    if src == 0 then
+        CNF.methods.Log("error", "Console can't create a character because it needs a player id.")
+        return
+    end
+
+    
+end)
