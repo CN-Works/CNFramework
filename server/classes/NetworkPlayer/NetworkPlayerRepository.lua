8949ca0240ddd31e63cacbb350b5030d60b32e3d
ChernyyOrel <victor.buzenco@epitech.eu>
2025-12-13 20:27:25 +0100

    Fix : merge issues

--- server/classes/NetworkPlayer/NetworkPlayerRepository.lua
+++ server/classes/NetworkPlayer/NetworkPlayerRepository.lua
@@ -12,34 +12,13 @@ function Repository:getNetworkPlayers() -- table
     return self.private.networkPlayers
 end
 
--- serverId : int
--- playerId : int
-function Repository:createNetworkPlayer(serverId, playerId) -- NetworkPlayer / false
-    if not CNF.methods.ValidateType(serverId, "number") or serverId < 1 then
-        error("NetworkPlayerRepository:createNetworkPlayer invalid serverId input.")
-    end
-    
-    if not CNF.methods.ValidateType(playerId, "number") or playerId < 1 then
-        error("NetworkPlayerRepository:createNetworkPlayer invalid playerId input.")
-    end
-    
-    local networkPlayer = CNF.classes["NetworkPlayer"]:new(serverId, playerId)
-
-    -- Adds the player to the repository
-    self:addNetworkPlayer(networkPlayer)
-
-    return networkPlayer
-end
-
 -- networkPlayer : NetworkPlayer
 function Repository:addNetworkPlayer(networkPlayer) -- bool
     if not CNF.methods.ValidateType(networkPlayer, CNF.classes["NetworkPlayer"]) then
         error("NetworkPlayerRepository:addNetworkPlayer invalid networkPlayer input.")
     end
 
-    local serverId = CNF.classes["NetworkPlayer"]:getServerId()
-
-    CNF.methods.Log("debug",tostring("NetworkPlayerRepository:addNetworkPlayer serverId : "..serverId))
+    local serverId = networkPlayer:getServerId()
 
     self.private.networkPlayers[serverId] = networkPlayer
 
@@ -62,10 +41,29 @@ function Repository:getNetworkPlayerByPlayerId(playerId) -- NetworkPlayer / nil
     end
 
     for serverId, networkPlayer in pairs(self.private.networkPlayers) do
-        if playerId == CNF.classes["NetworkPlayer"]:getPlayerId() then
+        if playerId == networkPlayer:getPlayerId() then
             return networkPlayer
         end
     end
+end
+
+-- serverId : int
+-- playerId : int
+function Repository:createNetworkPlayer(serverId, playerId) -- NetworkPlayer / nil
+    if not CNF.methods.ValidateType(serverId, "number") or serverId < 1 then
+        error("NetworkPlayerRepository:createNetworkPlayer invalid serverId input.")
+    end
+    
+    if not CNF.methods.ValidateType(playerId, "number") or playerId < 1 then
+        error("NetworkPlayerRepository:createNetworkPlayer invalid playerId input.")
+    end
+    
+    local networkPlayer = CNF.classes["NetworkPlayer"]:new(serverId, playerId)
+
+    -- Adds the player to the repository
+    self:addNetworkPlayer(networkPlayer)
+
+    return networkPlayer
 end
 
 function Repository:init() -- bool
